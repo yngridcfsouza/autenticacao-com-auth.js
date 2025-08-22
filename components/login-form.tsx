cfa85d21263c607ea3554da2ddcbde9110686217
yngridcfsouza <isnotlive.yngrid@gmail.com>
2025-08-22 11:49:27 -0300

    feat: adds the signin and register pages
    adds the signin and register pages

---
+++ components/login-form.tsx
@@ -0,0 +1,76 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl">Acesse sua conta</CardTitle>
+          <CardDescription>
+            Insira seu e-mail abaixo para acessar sua conta!
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">E-mail</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@exemplo.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Senha</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Esqueceu sua senha?
+                  </a>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="********"
+                  required
+                />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full">
+                  Entrar
+                </Button>
+                <Button variant="outline" className="w-full">
+                  Entrar com o Google
+                </Button>
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Ainda não tem uma conta?{" "}
+              <a href="/register" className="underline underline-offset-4">
+                Registre-se
+              </a>
+              !
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
---
+++ components/register-form.tsx
@@ -0,0 +1,98 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export function RegisterForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl">Registre sua conta</CardTitle>
+          <CardDescription>
+            Faça sua conta para acessar nossos serviços!
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="name">Seu nome</Label>
+                <Input
+                  id="name"
+                  type="text"
+                  placeholder="Digite seu nome"
+                  required
+                />
+              </div>
+
+              <div className="grid gap-3">
+                <Label htmlFor="email">E-mail</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@exemplo.com"
+                  required
+                />
+              </div>
+
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Senha</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Esqueceu sua senha?
+                  </a>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="********"
+                  required
+                />
+              </div>
+
+              <div className="grid gap-3">
+                <Label htmlFor="confirmPassword">Confirme sua senha</Label>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="********"
+                  required
+                />
+              </div>
+
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full">
+                  Registrar
+                </Button>
+                <Button variant="outline" className="w-full">
+                  Registrar com o Google
+                </Button>
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Já tem uma conta?{" "}
+              <a href="/login" className="underline underline-offset-4">
+                Entre agora
+              </a>
+              !
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
